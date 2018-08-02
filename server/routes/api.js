a76a1f90cd41af1adc99d4cb635566ed1a01d18f
Vladislav Klochkov <Uladzislau_Klachkou@epam.com>
2018-08-02 11:16:11 +0300

    Removed package-lock.json

---
+++ server/routes/api.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+
+router.get('/', (req, res) => {
+    'use strict';
+    console.log('Route: /; API works!');
+    res.send('API works!');
+});
+
+module.exports = router;
